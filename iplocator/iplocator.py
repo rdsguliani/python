7f3b7f9ff7b0300f946239c5d13e0813aecd22cf
Ramandeep Guliani <ramandeep.guliani@ge.com>
2019-08-07 05:03:37 -0700

    added system-utilit

--- iplocator/iplocator.py
+++ iplocator/iplocator.py
@@ -1,2 +1,22 @@
+# libraries required
+# pip install python-geoip
+# pip install python-geoip-geolite2
 
-import pygeoip
+
+from geoip import geolite2
+
+match = geolite2.lookup('34.216.171.68');
+
+# print(match.country);
+#
+# for key in match:
+#     print (key, match[key])
+import maxminddb
+
+reader = maxminddb.open_database('GeoLite2-City.mmdb')
+
+details = reader.get('34.216.171.68')
+print(details);
+# {'country': ... }
+
+reader.close()
